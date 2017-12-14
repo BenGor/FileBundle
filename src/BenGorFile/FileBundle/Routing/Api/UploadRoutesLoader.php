4b9800e9394e08f1c9a7ebff8298d116695024de
benatespina <benatespina@gmail.com>
2017-12-14 15:04:26 +0100

    Changed get for post in the upload endopoint

--- src/BenGorFile/FileBundle/Routing/Api/UploadRoutesLoader.php
+++ src/BenGorFile/FileBundle/Routing/Api/UploadRoutesLoader.php
@@ -39,7 +39,7 @@ class UploadRoutesLoader extends RoutesLoader
                 [],
                 '',
                 [],
-                ['GET']
+                ['POST']
             )
         );
     }
